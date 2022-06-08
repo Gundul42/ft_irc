a19a1988e777b8b5a95399dabb459a875e2aad33
Gundul <sampah@mailbox.org>
2022-06-08 17:09:07 +0200

    two styles

---
+++ newstyle.cpp
@@ -0,0 +1,53 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <iostream>
+
+#define PORT "1974"
+
+int	main(void)
+{
+		struct addrinfo hints, *res;
+		int				sockfd;
+
+		memset(&hints, 0, sizeof hints);
+		hints.ai_family = AF_UNSPEC;		// AF_INET or AF_INET6 to force version
+		hints.ai_socktype = SOCK_STREAM;	// TCP
+		hints.ai_flags = AI_PASSIVE;		//system enters IP
+
+		getaddrinfo(NULL, PORT, &hints, &res);
+
+		//get socket fd
+		sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+		if (sockfd < 0)
+		{
+				std::cout << "Error: could not open socket" << std::endl;
+				exit(EXIT_FAILURE);
+		}
+		else
+				std::cout << "Success: Socket fd is : " << sockfd << std::endl;
+
+		//bind port to address
+		if (bind(sockfd, res->ai_addr, res->ai_addrlen) < 0)
+		{
+				std::cout << "Error: failed to bind port to addr" << std::endl;
+				exit(EXIT_FAILURE);
+		}
+		else
+				std::cout << "Success: bound to Port #" << PORT << std::endl;
+
+		//listen to incoming calls
+		if (listen(sockfd, 5) < 0)
+		{
+				std::cout << "Error: failed to listen on port #" << PORT << std::endl;
+				exit(EXIT_FAILURE);
+		}
+		else
+				std::cout << "Success: listen on Port #" << PORT << std::endl;
+
+
+		return (0);
+}
--- oldstyle.cpp
+++ oldstyle.cpp
@@ -2,6 +2,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <string.h>
 #include <iostream>
 
 #define PORT 1974
@@ -22,11 +23,13 @@ int	main(void)
 		else
 		{
 				std::cout << "socket succesfully opened" << std::endl;
-				std::cout << "socket nummber is " << sfd << std::endl;
+				std::cout << "socket number is " << sfd << std::endl;
 		}
 		
 		//init the sockaddr structure
-		srv.sin_family = AF_INET;		
+		//set all to zero
+		memset(&srv, 0, sizeof(srv));
+		srv.sin_family = AF_INET;
 		srv.sin_port = htons(PORT);			//convert from host byte order 2 network byte order
 		srv.sin_addr.s_addr = inet_addr("127.0.0.1"); //localhost
 		//srv.sin_addr.s_addr = INADDR_ANY; //macro for localhost equals the above
@@ -41,6 +44,17 @@ int	main(void)
 		}
 		else
 				std::cout << "SUCCESS bound to local port" << std::endl;
-
+		// 
+		// listen to request from client
+		nRet = listen(sfd, 5);
+		if (nRet < 0)
+		{
+				std::cout << "Failed to listen to local port" << std::endl;
+				exit(EXIT_FAILURE);
+		}
+		else
+				std::cout << "SUCCESS listening to local port" << std::endl;
+		//
+		// Keep waiting for new requests and proceed as per the request
 		return (0);
 }
