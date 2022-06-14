94a32ee4b2768f2617e4b3c72136234acf2f8c47
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2022-06-14 23:04:23 +0200

    bridge commands

--- IrcServ.cpp
+++ IrcServ.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   IrcServ.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: graja <graja@student.42wolfsburg.de>       +#+  +:+       +#+        */
+/*   By: mwen <mwen@student.42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 18:15:03 by graja             #+#    #+#             */
-/*   Updated: 2022/06/14 15:02:19 by graja            ###   ########.fr       */
+/*   Updated: 2022/06/14 23:01:42 by mwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,43 @@ IrcServ::IrcServ(const char *port)
         exit(1);
 	}
 	std::cout << "Success: Socket opened and listening at FD#" << socketfd << std::endl;
+
+	serviceCommands["KILL"] = &IrcServ::kill;
+	serviceCommands["NICK"] = &IrcServ::nick;
+	serviceCommands["NOTICE"] = &IrcServ::notice;
+	serviceCommands["OPER"] = &IrcServ::oper;
+	serviceCommands["PASS"] = &IrcServ::pass;
+	serviceCommands["PING"] = &IrcServ::ping;
+	serviceCommands["PONG"] = &IrcServ::pong;
+	serviceCommands["PRIVMSG"] = &IrcServ::privmsg;
+	serviceCommands["QUIT"] = &IrcServ::quit;
+	serviceCommands["SERVICE"] = &IrcServ::service;
+	serviceCommands["SERVLIST"] = &IrcServ::servlist;
+	serviceCommands["SQUERY"] = &IrcServ::squery;
+	serviceCommands["USER"] = &IrcServ::user;
+	serviceCommands["WHO"] = &IrcServ::who;
+	serviceCommands["WHOIS"] = &IrcServ::whois;
+	serviceCommands["WHOWAS"] = &IrcServ::whowas;
+	userCommands = serviceCommands;
+	userCommands["AWAY"] = &IrcServ::away;
+	userCommands["DIE"] = &IrcServ::die;
+	userCommands["INFO"] = &IrcServ::info;
+	userCommands["INVITE"] = &IrcServ::invite;
+	userCommands["JOIN"] = &IrcServ::join;
+	userCommands["KICK"] = &IrcServ::kick;
+	userCommands["LIST"] = &IrcServ::list;
+	userCommands["LUSERS"] = &IrcServ::lusers;
+	userCommands["MODE"] = &IrcServ::mode;
+	userCommands["MOTD"] = &IrcServ::motd;
+	userCommands["NAMES"] = &IrcServ::names;
+	userCommands["PART"] = &IrcServ::part;
+	userCommands["REHASH"] = &IrcServ::rehash;
+	userCommands["RESTART"] = &IrcServ::restart;
+	userCommands["STATS"] = &IrcServ::stats;
+	userCommands["TIME"] = &IrcServ::time;
+	userCommands["TOPIC"] = &IrcServ::topic;
+	userCommands["USERHOST"] = &IrcServ::userhost;
+	userCommands["VERSION"] = &IrcServ::version;
 }
 
 IrcServ::~IrcServ(void)
@@ -175,6 +212,7 @@ void IrcServ::loop(void)
 					else
 					{
 						std::cout << "fd#" << pfds[i].fd << " - " << nbytes << ": " << buf;
+						handle_command(pfds[i].fd, buf);
 						//we have received something
 						// client fd is pfds[i].fd
 						// string is buf
@@ -188,3 +226,57 @@ void IrcServ::loop(void)
 	}
 }
 
+void	IrcServ::handle_command(int socket, const void* buf)
+{
+	std::string			command;
+	std::stringstream	str(static_cast<const char*>(buf));
+
+	std::getline(str, command, ' ');
+	if (*(command.end() - 1) == '\n')
+		command.erase(command.find('\n'));
+		
+	userCommandsMap::const_iterator it = userCommands.find(command);
+	if (it == userCommands.end())
+	{
+		if (send(socket, ERR_BUFF, sizeof(ERR_BUFF), 0) == -1)
+			perror("send");
+	}
+	else
+		(this->*(it->second))();
+}
+
+int		IrcServ::away() { return 1; }
+int		IrcServ::die() { return 1; }
+int		IrcServ::info() { return 1; }
+int		IrcServ::invite() { return 1; }
+int		IrcServ::join() { return 1; }
+int		IrcServ::kick() { return 1; }
+int		IrcServ::kill() { return 1; }
+int		IrcServ::list() { return 1; }
+int		IrcServ::lusers() { return 1; }
+int		IrcServ::mode() { return 1; }
+int		IrcServ::motd() { return 1; }
+int		IrcServ::names() { return 1; }
+int		IrcServ::nick() { return 1; }
+int		IrcServ::notice() { return 1; }
+int		IrcServ::oper() { return 1; }
+int		IrcServ::part() { return 1; }
+int		IrcServ::pass() { return 1; }
+int		IrcServ::ping() { return 1; }
+int		IrcServ::pong() { return 1; }
+int		IrcServ::privmsg() { return 1; }
+int		IrcServ::quit() { return 1; }
+int		IrcServ::rehash() { return 1; }
+int		IrcServ::restart() { return 1; }
+int		IrcServ::service() { return 1; }
+int		IrcServ::servlist() { return 1; }
+int		IrcServ::squery() { return 1; }
+int		IrcServ::stats() { return 1; }
+int		IrcServ::time() { return 1; }
+int		IrcServ::topic() { return 1; }
+int		IrcServ::user() { return 1; }
+int		IrcServ::userhost() { return 1; }
+int		IrcServ::version() { return 1; }
+int		IrcServ::who() { return 1; }
+int		IrcServ::whois() { return 1; }
+int		IrcServ::whowas() { return 1; }
--- IrcServ.hpp
+++ IrcServ.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   IrcServ.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: graja <graja@student.42wolfsburg.de>       +#+  +:+       +#+        */
+/*   By: mwen <mwen@student.42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 18:04:16 by graja             #+#    #+#             */
-/*   Updated: 2022/06/14 14:16:16 by graja            ###   ########.fr       */
+/*   Updated: 2022/06/14 20:23:48 by mwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,32 @@
 #include <poll.h>
 #include <iostream>
 #include <map>
+#include <set>
 #include "ftClient.hpp"
+#include <string>
+#include <sstream>
+
+#define ERR_BUFF "You cannot write text in this buffer\n"
 
 class IrcServ
 {
 		private:
-				int	socketfd;
+				typedef int (IrcServ::*UserCommandPointer)();
+				typedef std::map<std::string, UserCommandPointer> userCommandsMap;
+				typedef std::map<std::string, UserCommandPointer> serviceCommandsMap;
+
+				int					socketfd;
+				userCommandsMap		userCommands;
+				serviceCommandsMap	serviceCommands;
+
 				IrcServ(const IrcServ & cpy);
 
 				IrcServ & operator=(const IrcServ & rgt);
 
-				void add_to_pfds( pollfd *pfds[], int newfd, int *fd_count, int *fd_size);
-				void del_from_pfds(pollfd pfds[], int i, int *fd_count);
-				void *get_in_addr(sockaddr *sa);
+				void	add_to_pfds( pollfd *pfds[], int newfd, int *fd_count, int *fd_size);
+				void	del_from_pfds(pollfd pfds[], int i, int *fd_count);
+				void	*get_in_addr(sockaddr *sa);
+				void	handle_command(int socket, const void* buf);
 
 		public:
 				IrcServ(const char *port);
@@ -41,4 +54,41 @@ class IrcServ
 
 				int		getSocketFd(void) const;
 				void	loop(void);
+
+				//commands, to be implemented
+				int		away();
+				int		die();
+				int		info();
+				int		invite();
+				int		join();
+				int		kick();
+				int		kill();
+				int		list();
+				int		lusers();
+				int		mode();
+				int		motd();
+				int		names();
+				int		nick();
+				int		notice();
+				int		oper();
+				int		part();
+				int		pass();
+				int		ping();
+				int		pong();
+				int		privmsg();
+				int		quit();
+				int		rehash();
+				int		restart();
+				int		service();
+				int		servlist();
+				int		squery();
+				int		stats();
+				int		time();
+				int		topic();
+				int		user();
+				int		userhost();
+				int		version();
+				int		who();
+				int		whois();
+				int		whowas();
 };
