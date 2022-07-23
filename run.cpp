ffd1cd8fc2fbba0457a49f0b87f89e2688986324
Gundul <sampah@mailbox.org>
2022-07-23 14:12:27 +0200

    last

--- run.cpp
+++ run.cpp
@@ -6,7 +6,7 @@
 /*   By: mwen <mwen@student.42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 18:52:50 by graja             #+#    #+#             */
-/*   Updated: 2022/07/08 17:01:49 by graja            ###   ########.fr       */
+/*   Updated: 2022/07/19 12:01:56 by graja            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,12 @@
 #include "ftClient.hpp"
 int	main(int argc, char **argv)
 {
-	std::cout << argc << std::endl;
 	if (argc != 3)
 	{
 			std::cout << argv[0] << " [PORT] [PASSWORD]" << std::endl;
 			exit (1);
 	}
-
 	IrcServ	srv(argv[1], argv[2]);
-
 	srv.loop();
-
 	return(0);
 }
